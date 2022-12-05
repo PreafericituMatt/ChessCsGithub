379a9e8f5e518e38246e7bd6c3857573b2887fe0
Matei Ghizasan <matei.ghizasan@itperspectives.ro>
2022-12-05 16:32:31 +0200

    xaxaxax

---
+++ CSharpChess-master/Chess/ChessPiece.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public abstract class ChessPiece
+    {
+        protected const int MAX_DISTANCE = 7;
+
+        // Pawn fields
+        protected bool canEnPassantLeft;
+        protected bool canEnPassantRight;
+        protected bool canDoubleJump;
+
+        // Other fields
+        protected bool canCastle; // For rooks and kings
+        protected Point[][] availableMoves; // Array for moves ([direction][distance])
+        protected Point[][] availableAttacks; // Same as Moves except pawn
+        private int player;
+
+        public Point[][] AvailableMoves
+        {
+            get { return availableMoves; }
+        }
+
+        public Point[][] AvailableAttacks
+        {
+            get { return availableAttacks; }
+        }
+
+        public int Player
+        {
+            get { return player; }
+            set { player = value; }
+        } 
+
+        public abstract ChessPiece CalculateMoves();
+        
+        // Get relative horizontal or virtical movement coordinates, king , queen , pawn , rook
+       
+        // distance = How far in the given direction
+        // direction = Direction relative to player
+        // Returns an array for horizontal or vertical movement
+        public static Point[] GetMovementArray(int distance, Direction direction)
+        {
+            Point[] movement = new Point[distance];
+            int xPosition = 0;
+            int yPosition = 0;
+
+            for (int i = 0; i < distance; i++)
+            {
+                switch (direction)
+                {
+                    case Direction.FORWARD:
+                        yPosition++;
+                        break;
+                    case Direction.BACKWARD:
+                        yPosition--;
+                        break;
+                    case Direction.LEFT:
+                        xPosition++;
+                        break;
+                    case Direction.RIGHT:
+                        xPosition--;
+                        break;
+                    default:
+                        break;
+                }
+                movement[i] = new Point(xPosition, yPosition);
+            }
+            return movement;
+        }
+
+    
+        // Get relative diagnal movement coordinates used by king, queen, pawn, bishop
+        public static Point[] GetDiagnalMovementArray(int distance, DiagnalDirection direction)
+        {
+            Point[] attack = new Point[distance];
+            int xPosition = 0;
+            int yPosition = 0;
+
+            for (int i = 0; i < distance; i++)
+            {
+                switch (direction)
+                {
+                    case DiagnalDirection.FORWARD_LEFT:
+                        xPosition--;
+                        yPosition++;
+                        break;
+                    case DiagnalDirection.FORWARD_RIGHT:
+                        xPosition++;
+                        yPosition++;
+                        break;
+                    case DiagnalDirection.BACKWARD_LEFT:
+                        xPosition--;
+                        yPosition--;
+                        break;
+                    case DiagnalDirection.BACKWARD_RIGHT:
+                        xPosition++;
+                        yPosition--;
+                        break;
+                    default:
+                        break;
+                }
+                attack[i] = new Point(xPosition, yPosition);
+            }
+            return attack;
+        }
+    }
+}
---
+++ CSharpChess-master/Chess/Enumorators.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public struct Point
+    {
+        public int x;
+        public int y;
+        // <param name="x">The horazontal position</param>
+        // <param name="y">The virtical position</param>
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+      
+        // Display coordinates as a string
+        //Returns coordinates in the format (x, y)
+        public override string ToString()
+        {
+            return (String.Format("({0}, {1})", x, y));
+        }
+    }
+
+    public enum Direction
+    {
+        FORWARD,
+        BACKWARD,
+        LEFT,
+        RIGHT
+    }
+
+    public enum DiagnalDirection
+    {
+        FORWARD_LEFT,
+        FORWARD_RIGHT,
+        BACKWARD_LEFT,
+        BACKWARD_RIGHT
+    }
+}
